27792f6381557e1954a5382223a5923a6ae10c1d
Alkamov <ytiagobr@gmail.com>
2021-03-23 18:46:51 -0300

    111

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-alert(name)
 var name = 'Tiago'
+
+var idadeUsuario = prompt("How old are you?")
+idadeUsuario = parseInt(idadeUsuario)
+idadeUsuario += 1
+
+alert('Next year you will have ' + idadeUsuario)
